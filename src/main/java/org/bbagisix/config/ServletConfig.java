e3797892038c0ae663e635dcaf689a467626544b
hoonssac <Hoonssac.dev@gmail.com>
2025-07-15 17:18:41 +0900

    Initial commit

---
+++ src/main/java/org/bbagisix/config/ServletConfig.java
@@ -0,0 +1,43 @@
+package org.bbagisix.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@EnableWebMvc // FrontController (DispatcherServlet)
+@ComponentScan(basePackages = {"org.bbagisix.controller"}) // Spring MVC용 컴포넌트 등록을 위한 스캔 패키지
+public class ServletConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry
+			.addResourceHandler("/resources/**") // url이 /resources/로 시작하는 모든 경로 (정작 파일 등록)
+			.addResourceLocations("/resources/"); // webapp/resources/ 경로로 매핑
+		
+	}
+
+	// jsp view resolver 설정
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		InternalResourceViewResolver bean = new InternalResourceViewResolver();
+
+		bean.setViewClass(JstlView.class);
+		bean.setPrefix("/WEB-INF/views/");
+		bean.setSuffix(".jsp");
+
+		registry.viewResolver(bean);
+	}
+
+	@Bean
+	public MultipartResolver multipartResolver() {
+		StandardServletMultipartResolver resolver = new StandardServletMultipartResolver();
+		return resolver;
+	}
+}
