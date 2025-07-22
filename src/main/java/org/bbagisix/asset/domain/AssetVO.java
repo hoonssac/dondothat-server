820f866d1db824f59c8d54bf6519de7dea784c0e
msh <shmoon12@naver.com>
2025-07-22 16:10:12 +0900

    asset 코드 초기화

--- src/main/java/org/bbagisix/asset/domain/AssetVO.java
+++ src/main/java/org/bbagisix/asset/domain/AssetVO.java
@@ -1,18 +1,4 @@
 package org.bbagisix.asset.domain;
 
-import java.util.Date;
-
-import lombok.Data;
-
-@Data
 public class AssetVO {
-	private Long assetId;
-	private Long userId;
-	private String assetName;
-	private Long balance;
-	private String bankName;
-	private String bankAccount;
-	private String bankId;
-	private String bankPw;
-	private Date createdAt;
 }
--- src/main/java/org/bbagisix/asset/dto/AssetDTO.java
+++ src/main/java/org/bbagisix/asset/dto/AssetDTO.java
@@ -1,18 +1,4 @@
 package org.bbagisix.asset.dto;
 
-import java.util.Date;
-
-import lombok.Data;
-
-/* API를 통해 클라이언트에게 보여지는 DTO
- * ID와 PW 제외
- * */
-@Data
 public class AssetDTO {
-	private Long assetId;
-	private String assetName;
-	private Long balance;
-	private String bankName;
-	private String bankAccount;
-	private Date createdAt;
 }
