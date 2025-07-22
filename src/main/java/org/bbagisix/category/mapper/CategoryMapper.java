c7979a2dc0836b7242a6831b6a5441438510dd54
msh <shmoon12@naver.com>
2025-07-22 10:15:54 +0900

    CategoryMapper 인터페이스 및 XML 작성

--- src/main/java/org/bbagisix/category/mapper/CategoryMapper.java
+++ src/main/java/org/bbagisix/category/mapper/CategoryMapper.java
@@ -1,4 +1,13 @@
 package org.bbagisix.category.mapper;
 
+import java.util.List;
+
+import org.bbagisix.category.domain.CategoryVO;
+
 public interface CategoryMapper {
+	// 모든 카테고리 목록 조회
+	List<CategoryVO> findAll();
+
+	// id를 기준으로 카테고리 정보 조회
+	CategoryVO findById(Long categoryId);
 }
