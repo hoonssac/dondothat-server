d484c5a09e167236ec2c56912d64437550dcdf5e
msh <shmoon12@naver.com>
2025-07-22 13:06:42 +0900

    Expense VO,DTO 추가

--- src/main/java/org/bbagisix/expense/domain/ExpenseVO.java
+++ src/main/java/org/bbagisix/expense/domain/ExpenseVO.java
@@ -1,4 +1,18 @@
 package org.bbagisix.expense.domain;
 
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
 public class ExpenseVO {
+	private Long expenditureId;
+	private Long userId;
+	private Long categoryId;
+	private Long assetId;
+	private Long amount;
+	private String description;
+	private Date expenditureDate;
+	private Date createdAt;
+	private Date updatedAt;
 }
--- src/main/java/org/bbagisix/expense/dto/ExpenseDTO.java
+++ src/main/java/org/bbagisix/expense/dto/ExpenseDTO.java
@@ -1,4 +1,23 @@
 package org.bbagisix.expense.dto;
 
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
 public class ExpenseDTO {
+	// 응답 필드
+	private Long expenditureId;
+	private String categoryName;
+	private String categoryIcon;
+	private Date createdAt;
+	private Date updatedAt;
+
+	// 요청/응답 공통 필드
+	private Long userId;
+	private Long categoryId;
+	private Long assetId;
+	private int amount;
+	private String description;
+	private Date expenditureDate;
 }
