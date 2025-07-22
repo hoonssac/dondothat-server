71c2e9a7b8f2c6c3c6009a6dab3fbf9177ada276
msh <shmoon12@naver.com>
2025-07-22 13:32:27 +0900

    Asset VO, DTO 추가

--- src/main/java/org/bbagisix/asset/domain/AssetVO.java
+++ src/main/java/org/bbagisix/asset/domain/AssetVO.java
@@ -1,4 +1,18 @@
 package org.bbagisix.asset.domain;
 
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
 public class AssetVO {
+	private Long assetId;
+	private Long userId;
+	private String assetName;
+	private Long balance;
+	private String bankName;
+	private String bankAccount;
+	private String bankId;
+	private String bankPw;
+	private Date createdAt;
 }
--- src/main/java/org/bbagisix/asset/dto/AssetDTO.java
+++ src/main/java/org/bbagisix/asset/dto/AssetDTO.java
@@ -1,4 +1,18 @@
 package org.bbagisix.asset.dto;
 
+import java.util.Date;
+
+import lombok.Data;
+
+/* API를 통해 클라이언트에게 자산 정보 전달하는 DTO
+ * ID와 PW 제외
+ * */
+@Data
 public class AssetDTO {
+	private Long assetId;
+	private String assetName;
+	private Long balance;
+	private String bankName;
+	private String bankAccount;
+	private Date createdAt;
 }
