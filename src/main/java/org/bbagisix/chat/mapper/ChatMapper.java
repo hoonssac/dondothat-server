d12cee2e50add2e9f4e1a3b32121843bc87b1117
Hyunji-JANG <78188222+Hyunji-JANG@users.noreply.github.com>
2025-07-23 11:49:39 +0900

    Feat: #45 채팅 입/퇴장 기능을 위한 Mapper 로직 추가

--- src/main/java/org/bbagisix/chat/mapper/ChatMapper.java
+++ src/main/java/org/bbagisix/chat/mapper/ChatMapper.java
@@ -1,5 +1,7 @@
 package org.bbagisix.chat.mapper;
 
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.bbagisix.chat.dto.ChatMessageDTO;
@@ -12,4 +14,6 @@ public interface ChatMapper {
 
 	// 특정 메시지 조회 (사용자 정보 포함)
 	ChatMessageDTO selectMessageById(@Param("messageId") Long messageId);
+
+	Map<String, Object> selectUserById(@Param("userId") Long userId);
 }
