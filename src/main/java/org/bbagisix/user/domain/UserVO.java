6683775c1fbfc009ce7ebcec268683484ced82c4
hoonssac <Hoonssac.dev@gmail.com>
2025-07-24 11:46:04 +0900

    Feat: #49 User 도메인 작성

--- src/main/java/org/bbagisix/user/domain/UserVO.java
+++ src/main/java/org/bbagisix/user/domain/UserVO.java
@@ -1,4 +1,24 @@
 package org.bbagisix.user.domain;
 
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserVO {
+	private Long id;
+	private String email;
+	private String password;
+	private Long point;
+	private String nickname;
+	private LocalDateTime createdAt;
+	private LocalDateTime updatedAt;
+
+	private boolean email_verified;
+	private String emailVerificationToken;
+	private LocalDateTime emailVerificationTokenExpiry;
 }
