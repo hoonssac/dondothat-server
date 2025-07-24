12902bf27e6b4f67b0d8544371df5f1106f34933
westh07~ <westsunh@gmail.com>
2025-07-24 15:16:41 +0900

    AssetVO, AssetDTO 파일 변경 커밋

--- src/main/java/org/bbagisix/asset/domain/AssetVO.java
+++ src/main/java/org/bbagisix/asset/domain/AssetVO.java
@@ -1,4 +1,25 @@
 package org.bbagisix.asset.domain;
 
+import java.sql.Timestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class AssetVO {
+	// asset db
+	private Long assetId;
+	private Long userId;
+	private String assetName;
+	private Long balance;
+	private String bankName;
+	private String bankAccount;
+	private String bankId;
+	private String bankPw;
+	private String connectedId;
 }
--- src/main/java/org/bbagisix/asset/dto/AssetDTO.java
+++ src/main/java/org/bbagisix/asset/dto/AssetDTO.java
@@ -1,4 +1,17 @@
 package org.bbagisix.asset.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class AssetDTO {
+	private String bankName;
+	private String bankId;
+	private String bankpw;
+	private String bankAccount;
 }
