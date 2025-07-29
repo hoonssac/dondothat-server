963176faf1f73eff1317c26f6a53a7e77ecb266f
hoonssac <Hoonssac.dev@gmail.com>
2025-07-29 14:15:44 +0900

    Feat: #82 OAuth2 응답 인터페이즈

---
+++ src/main/java/org/bbagisix/oauth2/dto/OAuth2Response.java
@@ -0,0 +1,16 @@
+package org.bbagisix.oauth2.dto;
+
+public interface OAuth2Response {
+
+	// 제공자
+	String getProvider();
+
+	// 제공자에서 발급해주는 아이디
+	String getProviderId();
+
+	// 이메일
+	String getEmail();
+
+	// 사용자 실명
+	String getName();
+}
