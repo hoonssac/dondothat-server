4dfdbde806ea012336963991650173e2fae6e7ec
hoonssac <Hoonssac.dev@gmail.com>
2025-07-31 15:02:17 +0900

    Feat: #82 Jwt 필터 추가

---
+++ src/main/java/org/bbagisix/user/filter/JWTFilter.java
@@ -0,0 +1,61 @@
+package org.bbagisix.user.filter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.bbagisix.user.dto.CustomOAuth2User;
+import org.bbagisix.user.dto.UserDTO;
+import org.bbagisix.user.util.JWTUtil;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class JWTFilter extends OncePerRequestFilter {
+
+	private final JWTUtil jwtUtil;
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+		FilterChain filterChain) throws
+		ServletException, IOException {
+
+		String authorizationHeader = request.getHeader("Authorization");
+
+		if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer")) {
+			filterChain.doFilter(request, response);
+
+			return;
+		}
+
+		String token = authorizationHeader.split(" ")[1];
+
+		if (jwtUtil.isExpired(token)) {
+			System.out.println("token expired");
+			filterChain.doFilter(request, response);
+			return;
+		}
+
+		String name = jwtUtil.getName(token);
+		String role = jwtUtil.getRole(token);
+
+		UserDTO userDTO = new UserDTO();
+		userDTO.setName(name);
+		userDTO.setRole(role);
+
+		CustomOAuth2User customOAuth2User = new CustomOAuth2User(userDTO);
+
+		Authentication authToken = new UsernamePasswordAuthenticationToken(customOAuth2User, null,
+			customOAuth2User.getAuthorities());
+
+		SecurityContextHolder.getContext().setAuthentication(authToken);
+		filterChain.doFilter(request, response);
+	}
+}
