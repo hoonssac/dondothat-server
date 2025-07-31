6604a23d7cc578d751f2098f686eb46799463408
hoonssac <Hoonssac.dev@gmail.com>
2025-07-31 15:07:21 +0900

    Feat: #82 사용자 정보 반환 메서드 추가

--- src/main/java/org/bbagisix/user/controller/UserController.java
+++ src/main/java/org/bbagisix/user/controller/UserController.java
@@ -2,11 +2,14 @@ package org.bbagisix.user.controller;
 
 import javax.validation.Valid;
 
+import org.bbagisix.user.dto.CustomOAuth2User;
 import org.bbagisix.user.dto.SendCodeRequest;
 import org.bbagisix.user.dto.SignUpRequest;
 import org.bbagisix.user.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/user")
 @RequiredArgsConstructor
 public class UserController {
 
@@ -31,5 +34,12 @@ public class UserController {
 	public ResponseEntity<String> signUp(@Valid @RequestBody SignUpRequest request) {
 		userServices.signUp(request);
 		return ResponseEntity.status(HttpStatus.CREATED).body("회원가입이 성공적으로 완료되었습니다.");
+	}
+
+	@GetMapping("/me")
+	public ResponseEntity<?> getMyInfo(@AuthenticationPrincipal CustomOAuth2User customOAuth2User) {
+		String name = customOAuth2User.getName();
+		String role = customOAuth2User.getRole();
+		return ResponseEntity.ok("name: " + name + ", role: " + role);
 	}
 }
