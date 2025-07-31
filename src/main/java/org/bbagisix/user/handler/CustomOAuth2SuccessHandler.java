ba177364ab14c48eb7540c91acd43b1238e88094
hoonssac <Hoonssac.dev@gmail.com>
2025-07-31 22:23:54 +0900

    Feat: #82 Jwt 토큰을 파라미터에 담아 프론트엔드로 전송

--- src/main/java/org/bbagisix/user/handler/CustomOAuth2SuccessHandler.java
+++ src/main/java/org/bbagisix/user/handler/CustomOAuth2SuccessHandler.java
@@ -5,12 +5,14 @@ import org.bbagisix.user.util.JwtUtil;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @Component
 @RequiredArgsConstructor
@@ -22,12 +24,20 @@ public class CustomOAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHa
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         String token = jwtUtil.createToken(authentication.getName(), "ROLE_USER", 60 * 60 * 1000L); // 1시간 유효
 
-        Cookie cookie = new Cookie("jwt", token);
-        cookie.setPath("/");
-        cookie.setHttpOnly(true);
-        cookie.setSecure(request.isSecure());
-        response.addCookie(cookie);
+        String targetUrl = UriComponentsBuilder.fromUriString(
+                "http://localhost:5173/oauth-redirect")
+            .queryParam("token", token)
+            .build()
+            .encode(StandardCharsets.UTF_8)
+            .toUriString();
 
-        getRedirectStrategy().sendRedirect(request, response, "/oauth2-success");
+        // Cookie cookie = new Cookie("jwt", token);
+        // cookie.setPath("/");
+        // cookie.setHttpOnly(true);
+        // cookie.setSecure(request.isSecure());
+        // response.addCookie(cookie);
+
+        // getRedirectStrategy().sendRedirect(request, response, "/oauth2-success");
+        getRedirectStrategy().sendRedirect(request, response, targetUrl);
     }
 }
