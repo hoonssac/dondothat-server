1be2a176d178749e4f97590806d69b8c434c694a
hoonssac <Hoonssac.dev@gmail.com>
2025-08-03 01:37:12 +0900

    Refactor: #106 불필요한 import문 제거

--- src/main/java/org/bbagisix/user/service/UserService.java
+++ src/main/java/org/bbagisix/user/service/UserService.java
@@ -11,8 +11,6 @@ import org.bbagisix.user.dto.UserResponse;
 import org.bbagisix.user.mapper.UserMapper;
 import org.bbagisix.user.util.CookieUtil;
 import org.bbagisix.user.util.JwtUtil;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
