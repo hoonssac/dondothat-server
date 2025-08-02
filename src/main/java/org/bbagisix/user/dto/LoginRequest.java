4011a0490389e19b9a0bbddb19c74996b539f677
hoonssac <Hoonssac.dev@gmail.com>
2025-08-03 01:36:44 +0900

    Feat: #106 로그인 유효성 검사 추가

--- src/main/java/org/bbagisix/user/dto/LoginRequest.java
+++ src/main/java/org/bbagisix/user/dto/LoginRequest.java
@@ -1,9 +1,14 @@
 package org.bbagisix.user.dto;
 
+import org.apache.logging.log4j.core.config.plugins.validation.constraints.NotBlank;
+
 import lombok.Getter;
 
 @Getter
 public class LoginRequest {
+	@NotBlank(message = "이메일은 필수입니다.")
 	private String email;
+
+	@NotBlank(message = "비밀번호는 필수입니다.")
 	private String password;
 }
