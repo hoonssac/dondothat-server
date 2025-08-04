c579aee003da757347745eef4dfdce21858307f6
westh07~ <westsunh@gmail.com>
2025-08-04 14:12:30 +0900

    feat: #104 scheduler db 연결 구현

--- src/main/java/org/bbagisix/asset/mapper/AssetMapper.java
+++ src/main/java/org/bbagisix/asset/mapper/AssetMapper.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.annotations.Param;
 import org.bbagisix.asset.domain.AssetVO;
 import org.bbagisix.expense.domain.ExpenseVO;
 
+import java.util.Date;
 import java.util.List;
 
 @Mapper
@@ -16,5 +17,21 @@ public interface AssetMapper {
 	int deleteUserAssetByUserIdAndStatus(@Param("userId") Long userId, @Param("status") String status);
 
 	int deleteExpensesByUserId(Long userId);
+
 	int insertExpenses(List<ExpenseVO> expenseVO);
+
+	// 모든 main 계좌 조회
+	List<AssetVO> selectAllMainAssets();
+
+	// 계좌 잔액 업데이트
+	void updateAssetBalance(Long assetId, Long newBalance);
+
+	// 중복 거래내역 개수 조회
+	int countDuplicateTransaction(
+		@Param("userId") Long userId,
+		@Param("assetId") Long assetId,
+		@Param("amount") Long amount,
+		@Param("description") String description,
+		@Param("expenditureDate") Date expenditureDate
+	);
 }
