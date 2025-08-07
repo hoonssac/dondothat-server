97d80e4b291351c4a04935134cb191775719ff01
hyungEee <jihyung772@gmail.com>
2025-08-07 17:33:18 +0900

    feat: 성공/실패/진척도계산 메서드 및 관련 매퍼 추가 #137

--- src/main/java/org/bbagisix/challenge/mapper/ChallengeMapper.java
+++ src/main/java/org/bbagisix/challenge/mapper/ChallengeMapper.java
@@ -5,19 +5,32 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.bbagisix.challenge.domain.ChallengeVO;
+import org.bbagisix.challenge.domain.UserChallengeVO;
 import org.bbagisix.challenge.dto.ChallengeProgressDTO;
 
 @Mapper
 public interface ChallengeMapper {
 	ChallengeVO findByChallengeId(@Param("challengeId") Long challengeId);
+
 	boolean hasActiveChallenge(@Param("userId") Long userId);
+
 	boolean existsUser(@Param("userId") Long userId);
+
 	boolean existsUserChallenge(@Param("challengeId") Long challengeId, @Param("userId") Long userId);
+
 	void joinChallenge(@Param("challengeId") Long challengeId, @Param("userId") Long userId);
 
 	// 추천 카테고리에 해당하는 챌린지들 조회
-	List<ChallengeVO> findChallengesByCategoryIds(@Param("categoryIds") List<Long> categoryIds, @Param("userId") Long userId);
+	List<ChallengeVO> findChallengesByCategoryIds(@Param("categoryIds") List<Long> categoryIds,
+		@Param("userId") Long userId);
 
 	// 사용자 챌린지 진척도 조회
 	ChallengeProgressDTO getChallengeProgress(@Param("userId") Long userId);
+
+	List<UserChallengeVO> getOngoingChallenges();
+
+	Long getCategoryByChallengeId(Long challengeId);
+
+	void updateChallenge(UserChallengeVO userChallenge);
+
 }
