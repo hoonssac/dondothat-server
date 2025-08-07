faa0321d1bee683244d1476a6e96aff7e87c4197
msh <shmoon12@naver.com>
2025-08-07 14:02:54 +0900

    feat: #132 금감원 api가 반환하는 JSON 응답 전체를 받는 FssApiResponseDTO 파일 생성

---
+++ src/main/java/org/bbagisix/finproduct/dto/FssApiResponseDTO.java
@@ -0,0 +1,43 @@
+package org.bbagisix.finproduct.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class FssApiResponseDTO {
+
+    @JsonProperty("result")
+    private ResultDto result;
+
+    @Getter
+    @Setter
+    public static class ResultDto {
+        @JsonProperty("prdt_div")
+        private String prdtDiv;
+
+        @JsonProperty("total_count")
+        private int totalCount;
+
+        @JsonProperty("max_page_no")
+        private int maxPageNo;
+
+        @JsonProperty("now_page_no")
+        private int nowPageNo;
+
+        @JsonProperty("err_cd")
+        private String errCd;
+
+        @JsonProperty("err_msg")
+        private String errMsg;
+
+        @JsonProperty("baseList")
+        private List<ProductBaseDto> baseList;
+
+        @JsonProperty("optionList")
+        private List<ProductOptionDto> optionList;
+    }
+}
