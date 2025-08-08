e64a0cc9eb7d6c4c599e67025dedbe4eaf623ce2
hyungEee <jihyung772@gmail.com>
2025-08-08 17:44:49 +0900

    feat: expenseMapper에 이전기간동안의 카테고리 소비내역 총계 불러오는 구문 추가 #154

--- src/main/java/org/bbagisix/expense/mapper/ExpenseMapper.java
+++ src/main/java/org/bbagisix/expense/mapper/ExpenseMapper.java
@@ -3,11 +3,12 @@ package org.bbagisix.expense.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.bbagisix.expense.domain.ExpenseVO;
 
 @Mapper
 public interface ExpenseMapper {
-	
+
 	void insert(ExpenseVO expense);
 
 	ExpenseVO findById(Long expenditureId);
@@ -21,4 +22,8 @@ public interface ExpenseMapper {
 	List<ExpenseVO> getRecentExpenses(Long userId);
 
 	List<Long> getTodayExpenseCategories(Long userId);
+
+	Long getSumOfPeriodExpenses(@Param("userId") Long userId,
+		@Param("categoryId") Long categoryId,
+		@Param("period") Long period);
 }
