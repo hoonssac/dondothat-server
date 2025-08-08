902029d34edc9b50c3207ce55088bb33336e6fa5
hoonssac <Hoonssac.dev@gmail.com>
2025-08-08 20:46:01 +0900

    [REFACTOR] SamsSite 설정, 브라우저가 쿠키를 신뢰하도록 설정

--- src/main/java/org/bbagisix/user/util/CookieUtil.java
+++ src/main/java/org/bbagisix/user/util/CookieUtil.java
@@ -1,53 +1,28 @@
 package org.bbagisix.user.util;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 
 public class CookieUtil {
-    
+
     private static final String JWT_COOKIE_NAME = "accessToken";
     private static final int COOKIE_MAX_AGE = 24 * 60 * 60; // 24시간 (초 단위)
 
     public static void addJwtCookie(HttpServletResponse response, String token) {
-        Cookie cookie = new Cookie(JWT_COOKIE_NAME, token);
-        cookie.setHttpOnly(true); // XSS 공격 방지
-        cookie.setSecure(false); // 개발환경에서는 false, 운영환경에서는 true로 변경
-        cookie.setPath("/"); // 모든 경로에서 쿠키 전송
-        cookie.setMaxAge(COOKIE_MAX_AGE); // 24시간
-        
-        // SameSite 속성 설정 (CSRF 공격 방지)
-        response.setHeader("Set-Cookie", 
-            cookie.getName() + "=" + cookie.getValue() + 
-            "; Path=" + cookie.getPath() + 
-            "; Max-Age=" + cookie.getMaxAge() + 
-            "; HttpOnly" +
-            (cookie.getSecure() ? "; Secure" : "") +
-            "; SameSite=Lax");
+        String cookieHeader = String.format(
+            "%s=%s; Path=/; Max-Age=%d; HttpOnly; Secure; SameSite=None",
+            JWT_COOKIE_NAME,
+            token,
+            COOKIE_MAX_AGE
+        );
+        response.addHeader("Set-Cookie", cookieHeader);
     }
 
     public static void deleteJwtCookie(HttpServletResponse response) {
-        Cookie cookie = new Cookie(JWT_COOKIE_NAME, "");
-        cookie.setHttpOnly(true);
-        cookie.setSecure(false);
-        cookie.setPath("/");
-        cookie.setMaxAge(0); // 즉시 만료
-        response.addCookie(cookie);
-        
-        response.addHeader("Set-Cookie",
-            JWT_COOKIE_NAME + "=" + 
-            "; Path=/" + 
-            "; Max-Age=0" + 
-            "; HttpOnly" +
-            "; SameSite=Lax" +
-            "; Expires=Thu, 01 Jan 1970 00:00:00 GMT");
-            
-        response.addHeader("Set-Cookie",
-            JWT_COOKIE_NAME + "=deleted" + 
-            "; Path=/" + 
-            "; Max-Age=0" + 
-            "; HttpOnly" +
-            "; SameSite=Lax");
-            
+        String cookieHeader = String.format(
+            "%s=; Path=/; Max-Age=0; HttpOnly; Secure; SameSite=None; Expires=Thu, 01 Jan 1970 00:00:00 GMT",
+            JWT_COOKIE_NAME
+        );
+        response.addHeader("Set-Cookie", cookieHeader);
         System.out.println("쿠키 삭제 요청 전송됨: " + JWT_COOKIE_NAME);
     }
 }
