3d5c983a966dbdc52c1ab9247cad7ddf861743cd
westh07~ <westsunh@gmail.com>
2025-08-12 10:32:33 +0900

    fix: #171 progressDTO userChallengeId 추가 is_active 제거

--- src/main/java/org/bbagisix/challenge/dto/ChallengeProgressDTO.java
+++ src/main/java/org/bbagisix/challenge/dto/ChallengeProgressDTO.java
@@ -6,11 +6,11 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class ChallengeProgressDTO {
+	private Long user_challenge_id;
 	private Long challenge_id;
 	private String title;
-	private Long period;
 	private String status;
+	private Long period;
 	private Long progress;
 	private Long saving;
-	private Boolean is_active;
 }
